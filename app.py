f38c4609915350f279cb266060f07fafccf26019
jck-bit <jckkinyanjui@gmail.com>
2021-05-06 18:06:55 +0300

    oerators

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
-print('hello world')
 
-name = 'john'
-print(name)
+#the input function allows you to write on the terminal
+
+print('what is your name?')
+name = input()
+print('hello', name)
+
+#operators
+num1 = 45
+num2 = 10
+print(num1+num2)
+
+#integer division
+#there is no remainder
+num3 = 64
+num4 = 10
+print(num3//num4)
