09ecf1d8023a96d8a3234dc81a330a282a775910
jck-bit <jckkinyanjui@gmail.com>
2021-05-09 23:23:31 +0300

    strip methd

--- app.py
+++ app.py
@@ -1,21 +1,4 @@
-#iterating through a list.
-#this is known as iterating by item
-fruits = ['mango', 'apple', 'strawberry', 'banana']
-
-for fruit in fruits:
-    print(fruit)
-
-#another example includes
-
-for fruit in fruits:
-    if fruit == 'banana':
-        print('banana')
-    else:
-        print('not a banana')
-
-#iterating using the indices method
-for x in range(0, 3):
-    if fruits[x] == 'banana':
-        print(fruits[x])
-    else:
-        print('not a banana')
+#string methods
+# the strip method is used to remove spacing 
+text = input('input sething: ')
+print(text.strip())
