612a86700831b1f9050557a0371142f8e0b7d974
jck-bit <jckkinyanjui@gmail.com>
2021-05-11 15:29:50 +0300

    functions in python

--- app.py
+++ app.py
@@ -1,4 +1,31 @@
-#string methods
-# the strip method is used to remove spacing 
-text = input('input sething: ')
-print(text.strip())
+#functions in python
+
+def addtwo(x):
+
+    return x + 2
+
+
+newnumber = addtwo(7)
+
+print(newnumber)
+
+
+def printstring(string):
+    print(string)
+
+
+printstring('hello world')
+
+printstring('my name is jack')
+
+
+
+def distance(speed, time):
+    d = speed * time
+    return d
+
+print(distance(3,5))
+
+def dosomething():
+    print('am fine')
+dosomething()
