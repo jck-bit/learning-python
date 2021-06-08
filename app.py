af34a54d2eecb149503e4d7f2a69be081921886f
jck-bit <jckkinyanjui@gmail.com>
2021-06-08 23:16:08 +0300

    stuck on classes

--- app.py
+++ app.py
@@ -2,23 +2,12 @@
 # a method applies an object to itself
 #a function takes an ob ject and aplies an operation to it
 
+class user:
+    pass
 
+user1 = user()
+user1.first_name = 'jack'
+user1.last_name = 'kinyanjui'
 
-
-class dog(object):
-    def __init__(self ,name, age):
-        self.name = name
-        self.age = age
-        print('nice you made a scarf')
-
-    def speak(self):
-        print('hi am', self.name,'and am' ,self.age, 'years old')
-
-
-"""the speak method automatically takes self"""
-jack = dog('jack' , 19)
-fred = dog('fred' ,5)
-jack.speak()
-fred.speak()
-
-print(jack.age)
+print(user1.first_name)
+print(user1.last_name)
