f60edfa6a757058a35bdefdbb3cc1fc582fb18f8
jck-bi <jckkinyanjui@gmail.com>
2021-07-06 17:51:47 +0300

    levelling up python

--- app.py
+++ app.py
@@ -3,11 +3,15 @@
 #a function takes an ob ject and aplies an operation to it
 
 class user:
-    pass
+   pass
 
 user1 = user()
 user1.first_name = 'jack'
 user1.last_name = 'kinyanjui'
 
-print(user1.first_name)
+print(user1.first_name) 
 print(user1.last_name)
+
+calculation_to_seconds = 20 * 60 * 60
+
+print("f *20 days are {20 *" {calculation_to_seconds})
