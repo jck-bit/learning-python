fb2967384fe63fbf0073e2e1f5456fd98463da69
jck-bi <jckkinyanjui@gmail.com>
2021-07-09 01:04:51 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,11 @@
+def hello():
+    print('hello function!')
+
+#this is keeping your code dry...by avoiding repeating yourselves
+
+
+hello()
+hello()
+hello()
+hello()
+hello()
--- loops_iterations.py
+++ loops_iterations.py
@@ -1,4 +1,13 @@
 
+nums = [1, 2, 3, 4, 5]
+
+# a loop inside another loop
+for num in nums:
+    for letter in 'abc':
+        print(letter, num)
+
+#while loop....they go until a certain condition is met i.e until the value becomes false
+
 x = 1
 
 while x < 10 :
